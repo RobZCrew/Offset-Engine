900d838479f52fe43883ab60fc31f3483df83993
AstroZ859 <robzgameplays@gmail.com>
2026-09-11 22:38:07 -0500

    bruh

--- source/funkin/system/Main.hx
+++ source/funkin/system/Main.hx
@@ -12,7 +12,7 @@ class Main extends Sprite {
     public function new() {
         super();
 
-        addChild(new FlxGame(1280, 720, funkin.states.InitState, 60, 60, true, false));
+        addChild(new FlxGame(1280, 720, funkin.states.InitialState, 60, 60, true, false));
 
         #if !mobile
         FlxG.mouse.useSystemCursor = true;
